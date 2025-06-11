3a604b610ac90b0efba5380f33a1106c065e1f0c
liverHawk <hawkyofkn5365@gmail.com>
2025-06-11 16:58:39 +0900

    mistake

--- src/flow_package/utils.py
+++ src/flow_package/utils.py
@@ -4,9 +4,9 @@ import numpy as np
 
 def to_tensor(state, device="cpu"):
     # state -> tensor
-    port = np.int64(state[1])
-    protocol = np.int64(state[2])
-    other = np.insert(state[3:], 0, state[0])
+    port = np.int64(state[0])
+    protocol = np.int64(state[1])
+    other = np.insert(state[2:], 0, state[0])
 
     tensor_port = torch.tensor([port], dtype=torch.long, device=device)
     tensor_protocol = torch.tensor([protocol], dtype=torch.long, device=device)
