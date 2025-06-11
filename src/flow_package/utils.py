2ca5ed8bbccab326aba299273043e95670b74b4a
liverHawk <hawkyofkn5365@gmail.com>
2025-06-11 17:04:11 +0900

    repair

--- src/flow_package/utils.py
+++ src/flow_package/utils.py
@@ -6,7 +6,8 @@ def to_tensor(state, device="cpu"):
     # state -> tensor
     port = np.int64(state[0])
     protocol = np.int64(state[1])
-    other = np.insert(state[2:], 0, state[0])
+    # other = np.insert(state[2:], 0, state[0])
+    other = np.array(state[2:], dtype=np.float32)
 
     tensor_port = torch.tensor([port], dtype=torch.long, device=device)
     tensor_protocol = torch.tensor([protocol], dtype=torch.long, device=device)
